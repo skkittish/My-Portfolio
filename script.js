63b38ea6f5f7ff63b881cf0e68ff1830132b3b53
skkittish <93314993+skkittish@users.noreply.github.com>
2022-06-30 00:30:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -26,3 +26,19 @@ if (!event.target.matches('dropbtn')){
     }
 }
 }
+
+
+const scrollUp = document.querySelector("#scroll-up")
+scrollUp.addEventListener("click", () =>{
+    window.scrollTo({
+        top: 0,
+        left:0,
+        behavior:"smooth",
+    });
+});
+
+const checkbox = document.querySelector("#checkbox");
+checkbox.addEventListener("change", () => {
+document.body.classList.toggle("dark")
+});
+
